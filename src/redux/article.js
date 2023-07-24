82beac7df4ef4d7c974325b4139d75b5ba95700b
Abdul Omar <abbcc123123@gmail.com>
2023-07-24 19:27:15 -0400

    s

--- src/redux/article.js
+++ src/redux/article.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
-
+//T
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
